fda92b10c8d1ca2a57b8ac91b64ca68ca2456566
“taushikk” <taushifmalik@gmail.com>
2020-05-10 22:03:09 +0530

    Selenium hub

--- web-driver-6-stand-alone-and-grid/src/test/java/com/testing/SeleniumHubTest.java
+++ web-driver-6-stand-alone-and-grid/src/test/java/com/testing/SeleniumHubTest.java
@@ -4,30 +4,18 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+public class SeleniumHubTest {
 
-public class SeleniumStandAloneTest {
-	@Test
-	public void basic() {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://localhost:8081/pages/index.html");
-		System.out.println(driver.getCurrentUrl());
-		System.out.println(driver.getTitle());
-		driver.quit();
-
-	}
-
-	@Test
-	public void standalone() throws MalformedURLException, InterruptedException {
+	@Test(threadPoolSize = 2, invocationCount = 4)
+	public void hub_chrome() throws MalformedURLException, InterruptedException {
 
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setBrowserName("chrome");
+		// capabilities.setPlatform(Platform.WIN10.WINDOWS);
 
 		// WebDriverManager.chromedriver().setup();
 		// WebDriver driver = new ChromeDriver();
@@ -36,15 +24,6 @@ public class SeleniumStandAloneTest {
 		// RemoteWebDriver
 		// Location of StandAloneserver
 		// Which browser? which OS?
-
-		// Nodes should register to http://192.168.0.109:4444/grid/register/
-		// Clients should connect to http://192.168.0.109:4444/wd/hub
-		//selenium-standalone start -- -role hub
-		// selenium-standalone start -- -role node -hub
-		// http://192.168.0.109:4444/grid/register/
-		// http://192.168.0.109:4444/grid/register/
-		// selenium-standalone start -- -role node -port 556 -hub
-		// http://192.168.0.109:4444/grid/register/
 
 		remoteDriver.get("http://localhost:8081/pages/index.html");
 		System.out.println(remoteDriver.getCurrentUrl());
@@ -55,10 +34,10 @@ public class SeleniumStandAloneTest {
 	}
 
 	@Test(threadPoolSize = 2, invocationCount = 4)
-	public void hub() throws MalformedURLException, InterruptedException {
+	public void hub_firefox() throws MalformedURLException, InterruptedException {
 
 		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setBrowserName("chrome");
+		capabilities.setBrowserName("firefox");
 
 		// WebDriverManager.chromedriver().setup();
 		// WebDriver driver = new ChromeDriver();
